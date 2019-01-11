2daa3cc5f06a7a286f41b7116bba67d49ee83636
Travis CI User <travis@example.org>
2019-01-11 15:42:35 +0000

    Automatic Travis documentation build

--- master/implementors/log/trait.Log.js
+++ master/implementors/log/trait.Log.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["kfs_kernel"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.6/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"kfs_kernel/log_impl/struct.Logger.html\" title=\"struct kfs_kernel::log_impl::Logger\">Logger</a>",synthetic:false,types:[]},];
+implementors["kfs_libuser"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.6/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"kfs_libuser/log_impl/struct.Logger.html\" title=\"struct kfs_libuser::log_impl::Logger\">Logger</a>",synthetic:false,types:[]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
